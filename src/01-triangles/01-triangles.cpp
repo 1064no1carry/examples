c2fd6751d4b27720e2ac3f36dd7df4b76fbf40bb
Yunxiang Zhang <yunxiang@Yunxiangs-MacBook-Pro.local>
2020-12-27 00:38:08 -0800

    fix glfw version

--- src/01-triangles/01-triangles.cpp
+++ src/01-triangles/01-triangles.cpp
@@ -88,8 +88,8 @@ main( int argc, char** argv )
     glfwInit();
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Triangles", NULL, NULL);
 
